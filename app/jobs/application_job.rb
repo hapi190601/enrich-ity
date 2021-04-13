1eb721e9a29ce85c372242157990ae4efe6149a3
hapi190601 <gc1206ro@gmail.com>
2021-04-13 10:44:49 +0000

    チャット機能自動更新完成

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,17 +1,2 @@
-class DirectMessageBroadcastJob < ApplicationJob
-  queue_as :default
-
-  def perform(direct_message)
-    # マルチスレッド処理対策？書いても効果なし
-    # Rails.application.eager_load!
-    ActionCable.server.broadcast "room_channel_#{direct_message.room_id}", message: render_direct_message(direct_message)
-  end
-
-  private
-
-  def render_direct_message(direct_message)
-    # マルチスレッド処理対策？書いても効果なし
-    # Rails.application.eager_load!
-    ApplicationController.renderer.render(partial: 'direct_messages/direct_message', locals: { direct_message: direct_message })
-  end
+class ApplicationJob < ActiveJob::Base
 end
