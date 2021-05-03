57de1dabf56145768c242f071d4a0b0e778e31ad
hapi190601 <gc1206ro@gmail.com>
2021-05-03 01:32:47 +0000

    バッチ処理時間変更(9時間差考慮)

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,11 +3,11 @@ set :output, 'log/crontab.log'
 set :environment, :production
 
 # 投稿掲載期限1ヶ月
-every 1.day, :at => '6:00 am' do
+every 1.day, :at => '11:00 pm' do
   runner "Post.monthly_destroy_all"
 end
 
 # ゲストユーザー毎日削除
-every 1.day, :at => '6:00 am' do
+every 1.day, :at => '11:00 pm' do
   runner "User.guest_delete"
 end
