ff9280e9eb03fad22bcf40f77ed31d4e9f2e44bf
hapi190601 <gc1206ro@gmail.com>
2021-05-13 05:37:03 +0000

    AddAdminUserIndex

--- app/controllers/private/users_controller.rb
+++ app/controllers/private/users_controller.rb
@@ -1,8 +1,15 @@
 class Private::UsersController < ApplicationController
   def index
+    @users = User.page(params[:page]).per(20)
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def out
