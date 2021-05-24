693b2a838102b99a4ad757af9e5a151ba4c95c8f
hapi190601 <gc1206ro@gmail.com>
2021-05-24 09:41:06 +0000

    管理者投稿用コントローラ修正

--- app/controllers/private/posts_controller.rb
+++ app/controllers/private/posts_controller.rb
@@ -1,10 +1,10 @@
 class Private::PostsController < ApplicationController
-  def index
-  end
-
-  def show
-  end
-
+  before_action :authenticate_admin!
+  
   def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    flash[:notice] = "投稿を削除しました。"
+    redirect_to user_path(current_user.id)
   end
 end
