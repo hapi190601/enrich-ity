a4112af50327a1bbc047e396f32f3518eeaea58c
hapi190601 <gc1206ro@gmail.com>
2021-06-03 11:21:01 +0000

    不要なコメント削除

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,7 +25,6 @@ document.addEventListener("turbolinks:before-cache", function() {
 });
 
 $(document).on('turbolinks:load', function() {
-  // トップへスクロールするアニメーション
   $('#up-scroll').hide();
   $(window).scroll(function(){
     $('#pos').text($(this).scrollTop());
